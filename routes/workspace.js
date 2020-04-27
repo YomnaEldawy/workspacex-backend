8621cf14434be53cfe453064228fd72557bb0185
yomna <yomna.eldawy@bosta.co>
2020-04-27 08:54:26 +0200

    add full details

--- routes/workspace.js
+++ routes/workspace.js
@@ -4,31 +4,47 @@ const executeQuery = require("../config/db");
 
 router.post("/new", async (req, res) => {
   try {
+    console.log(req.body);
     var ws_name = req.body.ws_name;
     var city = req.body.city;
     var phone = req.body.phone;
-
-    if (!ws_name || !phone) {
-      return res.send({ success: false });
-    }
-    console.log(req.body);
+    var streetName = req.body.streetName;
+    var streetNumber = req.body.streetNumber;
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var opensAt = req.body.opensAt;
+    var closesAt = req.body.closesAt;
+    var Day_to_open = req.body.Day_to_open;
+    var Day_to_close = req.body.Day_to_close;
     var query =
-      "insert into Workspace(name,city,phone, streetName, streetNumber) values ('" +
+      "insert into Workspace(name,city,phone,streetName, streetNumber,latitude,longitude,opensAt,closesAt,Day_to_open,Day_to_close) values ('" +
       ws_name +
       "','" +
       city +
       "','" +
       phone +
-      "'" +
-      `, '${req.body.streetName}', '${req.body.streetNumber}'` +
-      ");";
+      "','" +
+      streetName +
+      "','" +
+      streetNumber +
+      "','" +
+      latitude +
+      "','" +
+      longitude +
+      "','" +
+      opensAt +
+      "','" +
+      closesAt +
+      "','" +
+      Day_to_open +
+      "','" +
+      Day_to_close +
+      "');";
+    console.log(query);
     var result = await executeQuery(query);
-    var query2 = `update StaffMember set workspaceId=${result.insertId} where email='${req.body.loginDetails.userDetails.email}'`;
-    var result2 = await executeQuery(query2);
-    return res.send(result2);
+    res.send(result);
   } catch (err) {
-    console.log(err);
-    return res.send({ success: false });
+    res.send({ success: false });
   }
 });
 
